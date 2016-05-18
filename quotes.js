f5ccf92b3688ed7c17ca31f83118474eecb45fa4
Samiul Anwar <samiul.anwar@gmail.com>
2016-05-18 16:57:26 -0400

    making it easier to use random numbers to call a certain character

--- quotes.js
+++ quotes.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
 	var characters={
-		"jon":["http://i67.tinypic.com/v7rk3.png","#E4E4E4"],
-		"tyrion":["http://i65.tinypic.com/30di5fl.png","#D73224"],
-		"jaime":["http://i65.tinypic.com/30di5fl.png","#D73224"],
-		"brynden":["http://i66.tinypic.com/2i1kpba.png","#998BFA"],
-		"cersei":["http://i65.tinypic.com/30di5fl.png","#D73224"],
-		"hound":["http://i63.tinypic.com/2w5ung1.png","#FEE855"],
-		"littlefinger":["http://oi67.tinypic.com/oa9cu1.jpg","#B0CDFC"],
-		"olenna":["http://i65.tinypic.com/oh0gn5.png","#DAE8B6"],
-		"renly":["http://i68.tinypic.com/e7iknr.jpg","#EFAE2C"],
-		"varys":["http://i67.tinypic.com/2884j0g.png","#BF1E2E"],
-		"bran":["http://i67.tinypic.com/v7rk3.png","#E4E4E4"],
-		"victarion":["http://i68.tinypic.com/9gcxdu.png","#FBE106"],
-		"sansa":["http://i67.tinypic.com/v7rk3.png","#E4E4E4"],
-		"davos":["http://i68.tinypic.com/e7iknr.jpg","#EFAE2C"],
-		"daenerys":["http://i67.tinypic.com/2884j0g.png","#BF1E2E"]
+		1:["jon","http://i67.tinypic.com/v7rk3.png","#E4E4E4"],
+		2:["tyrion","http://i65.tinypic.com/30di5fl.png","#D73224"],
+		3:["jaime","http://i65.tinypic.com/30di5fl.png","#D73224"],
+		4:["brynden","http://i66.tinypic.com/2i1kpba.png","#998BFA"],
+		5:["cersei","http://i65.tinypic.com/30di5fl.png","#D73224"],
+		6:["hound","http://i63.tinypic.com/2w5ung1.png","#FEE855"],
+		7:["littlefinger","http://oi67.tinypic.com/oa9cu1.jpg","#B0CDFC"],
+		8:["olenna","http://i65.tinypic.com/oh0gn5.png","#DAE8B6"],
+		9:["renly","http://i68.tinypic.com/e7iknr.jpg","#EFAE2C"],
+		10:["varys","http://i67.tinypic.com/2884j0g.png","#BF1E2E"],
+		11:["bran","http://i67.tinypic.com/v7rk3.png","#E4E4E4"],
+		12:["victarion","http://i68.tinypic.com/9gcxdu.png","#FBE106"],
+		13:["sansa","http://i67.tinypic.com/v7rk3.png","#E4E4E4"],
+		14:["davos","http://i68.tinypic.com/e7iknr.jpg","#EFAE2C"],
+		15:["daenerys","http://i67.tinypic.com/2884j0g.png","#BF1E2E"]
 	};
 });
